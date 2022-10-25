21df48f76c57fa25bb7ef62cd855e25a80916f31
Alexander Pankoff <ccntrq@screenri.de>
2022-10-25 17:26:29 +0200

    Add doc comments to error.ts

--- src/errors.ts
+++ src/errors.ts
@@ -1,11 +1,22 @@
 import {bugsUrl, version} from './package';
 
+/**
+ * An error of this class will be thrown if any of the git operations fail.
+ */
 export class GitCmdError extends Error {}
 
+/**
+ * Typeguard to check if an unknown error is an instance of  {@link GitCmdError | `GitCmdError`}.
+ * @param error
+ */
 export function isGitCmdError(error: unknown): error is GitCmdError {
   return error instanceof Error && error.constructor.name === GitCmdError.name;
 }
 
+/**
+ * Catch all error class to wrap unknown errors. An error of this class popping
+ * up is considered a bug
+ */
 export class UnexpectedError extends Error {
   constructor(error: unknown) {
     super(
@@ -14,6 +25,11 @@ export class UnexpectedError extends Error {
     );
   }
 
+  /**
+   * Use this method to generate a url for an issue containing relevant
+   * information for this error.
+   * @returns clickable url to create an issue for this error
+   */
   newIssueUrl(): string {
     let url = `${bugsUrl}/new?title=`;
 
@@ -25,6 +41,10 @@ export class UnexpectedError extends Error {
   }
 }
 
+/**
+ * Typeguard to check if an unknown error is an instance of  {@link UnexpectedError | `UnexpectedError`}.
+ * @param error
+ */
 export function isUnexpectedError(error: unknown): error is UnexpectedError {
   return (
     error instanceof Error && error.constructor.name === UnexpectedError.name
